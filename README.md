39abdffb898c23bc57714b0ac63c096e7b4bfa45
Leonardo Ramos dos Santos <leonardo.ramos@santoslab.com>
2022-05-05 21:01:55 -0300

    Alterações para a nova versão do properties

---
+++ README.md
@@ -0,0 +1,10 @@
+# Locust
+
+Aplicativo para teste de carga para as APIs SantosLab.
+
+# Criando as tarefas de teste
+
+Cada API que será testada deve ter uma classe correspondente
+dentro do arquivo ```locustfile.py```.
+
+Cada chamada da API que será testada deve ter 
--- src/locustfile.py
+++ src/locustfile.py
@@ -1,6 +1,7 @@
 from locust import TaskSet, task
 from locust import HttpUser
 import os
+import json
 
 # Cada classe aqui declarada é uma API que deve ser chamada
 PROPERTIES_PORT = os.getenv("PROPERTIES_PORT", 5005)
@@ -12,37 +13,36 @@ class Properties(HttpUser):
     # Cada task é uma chamada dentro da API escolhida
     # Se for realizada autenticação, pode ser passada
     # como parâmetro dentro do data da requisição
-    @task
-    def health(self):
-        health_url = f"{self.host}:{PROPERTIES_PORT}/api/v1/health"
+    # @task
+    # def health(self):
+    #     health_url = f"{self.host}:{PROPERTIES_PORT}/api/v1/health"
 
-        self.client.get(health_url, name="properties-health")
+    #     self.client.get(health_url, name="properties-health")
 
     @task(1)
     def insert(self):
-        health_url = f"{self.host}:{PROPERTIES_PORT}/api/v1/insert"
+        insert_url = f"{self.host}:{PROPERTIES_PORT}/api/v1/insert"
 
         headers = {
-           'Content-Type': 'application/json'
+           "Content-Type": "application/json",
+           "Authorization": "abcd1234"
         }
         # Se for feita autenticação antes, pasar o token dentro do data
         data = {
-            "santos_token":"abcd1234",
-            "farmName":"Fazenda encanto",
-            "ownerName":"Roberto",
-            "car":"PI-2211209-344301157FAE4482A2884266E088DC59"
+            "farmName": "Fazenda encanto",
+            "ownerName": "Roberto",
+            "car": "PI-2211209-344301157FAE4482A2884266E088DC59"
         }
 
-        self.client.post(health_url, headers=headers, data=data,
-                         timeout=60000, name="properties-insert")
+        self.client.post(insert_url, headers=headers, data=json.dumps(data), name="properties-insert")
 
 
 ### API RESERVA LEGAL MAIS
-RESERVALEGALMAIS_PORT = os.getenv("RESERVALEGALMAIS_PORT", 5004)
-class ReservaLegalMais(HttpUser):
-    min_wait = 0
-    max_wait = 1000
+# RESERVALEGALMAIS_PORT = os.getenv("RESERVALEGALMAIS_PORT", 5004)
+# class ReservaLegalMais(HttpUser):
+#     min_wait = 0
+#     max_wait = 1000
 
-    @task
-    def health(self):
-        self.client.get("/api/v1/health", name="reservalegal-health")
+#     @task
+#     def health(self):
+#         self.client.get("/api/v1/health", name="reservalegal-health")
